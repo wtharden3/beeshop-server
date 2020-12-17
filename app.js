bfefca8a8add0b19334d42e83db35b0b1bae4015
Whitney Harden <wtharden320@gmail.com>
2020-12-16 22:07:51 -0500

    test on postman not work - added req and res parameters

--- app.js
+++ app.js
@@ -2,26 +2,26 @@ let express = require('express');
 let app = express();
 
 //for store endpoints - general use that does not require authorization/validation - must be first?
-app.use('/', () => {
+app.use('/', (req, res) => {
   res.send('This is the main endpoint. The store.');
 });
 
 //for user endponts - requires authorization/validation
-app.use('/user', () => {
+app.use('/user', (req, res) => {
   res.send(
     'This is the user endpoints. This will be used for login and signup'
   );
 });
 
 //for inventory endpoints - requires authorization/validation - requires role access/restriction
-app.use('/inventory', () => {
+app.use('/inventory', (req, res) => {
   res.send(
     'This is the inventory endpoint. This will be used for admin access to edit the inventory of products'
   );
 });
 
 //for order history
-app.use('/orderhistory', () => {
+app.use('/orderhistory', (req, res) => {
   res.send(
     'This is the orderhistory endpoint that admin and regular user can view and fully interact with.'
   );
