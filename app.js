ef4af14b6d972c3bc834e2e415dc8b7fb9fa6096
Whitney Harden <wtharden320@gmail.com>
2020-12-16 22:11:13 -0500

    endpoints working on app.js

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ let app = express();
 //for user endponts - requires authorization/validation
 app.use('/user', (req, res) => {
   res.send(
-    'This is the user endpoints. This will be used for login and signup'
+    'This is the USER endpoints. This will be used for login and signup'
   );
 });
 
 //for inventory endpoints - requires authorization/validation - requires role access/restriction
 app.use('/inventory', (req, res) => {
   res.send(
-    'This is the inventory endpoint. This will be used for admin access to edit the inventory of products'
+    'This is the INVENTORY endpoint. This will be used for admin access to edit the inventory of products'
   );
 });
 
 //for order history
 app.use('/orderhistory', (req, res) => {
   res.send(
-    'This is the orderhistory endpoint that admin and regular user can view and fully interact with.'
+    'This is the ORDERHISTORY endpoint that admin and regular user can view and fully interact with.'
   );
 });
 
