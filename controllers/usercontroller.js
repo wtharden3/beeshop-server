e4307c212e6464ba48d34076118e1d5e7079f8d8
Whitney Harden <wtharden320@gmail.com>
2020-12-18 16:00:58 -0500

    user register works with password encryption -bcrypt

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,33 +1,35 @@
 const router = require('express').Router();
 const { User } = require('../models');
 
+//the bcrypt module is needed to hash passwords - password encryption
+// we will use in register and login
+const bcrypt = require('bcryptjs');
+// the jwt module is needed for SESSION VALIDATION we will use it to create a token
+
 //get user data from user table - NOT CREATED YET
 router.post('/register', async (req, res) => {
-try{
-  const { firstName, lastName, email, password, isAdmin } = req.body;
-  //Create User
-  let newUser = await User.create({
-    firstName,
-    lastName,
-    email,
-    password,
-    isAdmin
-  });
-  res.status(200).json({
-    User: newUser,
-    message: `New Account Created`,
-    Admin: newUser.isAdmin 
-  })
-
-}catch(err){
-  res.status(500).json({
-    error: err,
-    message: "That's a no go on the user creation"
-    
-  })
-}
-    
-   
+  try {
+    //deconstructing the req.body properties
+    let { firstName, lastName, email, password, isAdmin } = req.body;
+    //Create User
+    let newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: bcrypt.hashSync(password, 13),
+      isAdmin,
+    });
+    res.status(201).json({
+      User: newUser,
+      message: `New Account Created`,
+      Admin: newUser.isAdmin,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+      message: "That's a no go on the user creation",
+    });
+  }
 });
 
 //export this module -- will send to index.js
