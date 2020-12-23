8ac10ffd31537dc7aef481c4f20e76870280cdd8
Whitney Harden <wtharden320@gmail.com>
2020-12-23 14:14:37 -0500

    product create works

--- controllers/productscontroller.js
+++ controllers/productscontroller.js
@@ -1,20 +1,36 @@
 const router = require('express').Router();
-const {Product} = require('../models')
+const { Product } = require('../models');
 
 // C - CREATE / POST
-router.post('/addtoinventory', (req,res) =>{
-  const  orderEntry = {productName, description, category, subCatetory, sku, adminId_fk: req.product.adminId_fk } = req.body;
+router.post('/addtoinventory', (req, res) => {
+  //console.log('from productcontroler-- req-->', req);
+  const orderEntry = {
+    productName: req.body.product.productName,
+    description: req.body.product.description,
+    category: req.body.product.category,
+    subCategory: req.body.product.subCategory,
+    sku: req.body.product.sku,
+    size: req.body.product.size,
+    adminId_fk: req.user.id,
+  };
+
   Product.create(orderEntry)
-  .then(order => res.status(200).json(order))
-  .catch(err => res.status(500).json({error: err, message: 'the route worked but something else did not'}))
-})
+    .then(order => res.status(200).json(order))
+    .catch(err =>
+      res
+        .status(500)
+        .json({
+          error: err,
+          message: 'the route worked but something else did not',
+        })
+    );
+});
 // R - READ / GET
 // U - UPDATE / PUT
-// D - DELETE 
-
+// D - DELETE
 
 //view all will need to be at the bottom
-router.get('/admin/inventory', (req, res) => {
+router.get('/inventory', (req, res) => {
   res.send(`I got whatcha want, I got what you need!`);
 });
 
