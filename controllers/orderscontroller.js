133a6ed30339bcce1b430a2b231e374882cd8e45
Whitney Harden <wtharden320@gmail.com>
2020-12-23 17:33:56 -0500

    place order works with intergers --no decimals

--- controllers/orderscontroller.js
+++ controllers/orderscontroller.js
@@ -1,17 +1,42 @@
 const router = require('express').Router();
+const { Order } = require('../models');
 
 //test
 // pop@email.com
 // pass
-// token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjA4MzQ2MTg1LCJleHAiOjE2MDg0MzI1ODV9.tVERXfYFotuPi_xaWO_cxepgb9dhaJsKWbb0okT-6CA
+
+// C - CREATE / POST
+router.post('/placeorder', (req, res) => {
+  let orderEntry = {
+    total: req.body.order.total,
+    //total: (req.body.order.subTotal * req.body.order.tax) + req.body.order.subTotal + req.body.order.shippingFee,
+    subTotal: req.body.order.subTotal,
+    tax: req.body.order.tax,
+    details: req.body.order.details,
+    shippingFee: req.body.order.shippingFee,
+    hasShipped: req.body.order.hasShipped,
+    customerId_fk: req.user.id,
+  };
+
+  Order.create(orderEntry)
+    .then(order =>
+      res.status(200).json({ order, message: 'Your order has ben placed' })
+    )
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: err, message: 'the route worked but check the data' })
+    );
+});
+// R - READ / GET
+// U - UPDATE / PUT
+// D - DELETE
 
 //protected route that only admin has access to
 router.get('/admin/all', (req, res) => {
   res.send(`I'm the biggest boss that you seen thus far --Rick Ross`);
 });
 
-
-
 //getby userid is next
 
-module.exports = router
+module.exports = router;
