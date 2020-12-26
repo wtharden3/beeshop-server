1a9715c9c0f6e57caf3b05f07d0b55b90a88e3d1
Whitney Harden <wtharden320@gmail.com>
2020-12-26 18:35:04 -0500

    orders update and delete working

--- controllers/orderscontroller.js
+++ controllers/orderscontroller.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const validateSession = require('../middleware/validateSession');
 const { Order } = require('../models');
-const { restore } = require('../models/user');
+//const { restore } = require('../models/user');
 
 //test
 // pop@email.com
@@ -46,13 +47,43 @@ router.get('/admin/orders', (req, res) => {
 });
 
 // get by userid
-router.get('/:id/orders', (req, res) => {
-  Order.findOne({ where: { id: req.params.id } }).then(myorders =>
+router.get('/:customerId/orders', (req, res) => {
+  Order.findAll({ where: { customerId_fk: req.params.customerId } }).then(myorders =>
     res.status(200).json({ myorders, message: 'Here is a list of YOUR orders' })
   );
 });
 // U - UPDATE / PUT
+router.put('/edit/:orderid', validateSession, (req, res) => {
+  //console.log('req----> ', req)
+  const updateOrder = {
+    total: req.body.order.total,
+    subTotal: req.body.order.subTotal,
+    tax: req.body.order.tax,
+    details: req.body.order.details,
+    shippingFee: req.body.order.shippingFee,
+    hasShipped: req.body.order.hasShipped
+  };
+  //console.log('req.body.order----> ', req.body.order)
+
+  const query = {where: {id: req.params.orderid}};
+
+  Order.update(updateOrder, query)
+  .then(order => {
+    if(order){
+      res.status(200).json({order, message: `${order} order(s) was updated`})
+    } else {
+      res.status(500).json({message: 'there are no orders to edit'})
+    }
+  })
+  .catch(err => res.status(500).json({error: err, message: 'there was an error searching or editing the order'}))
+})
 // D - DELETE
+router.delete('/delete/:orderid', (req, res) => {
+  const query = {where: {id: req.params.orderid}};
+  Order.destroy(query)
+  .then(() => res.status(200).json({message: 'This order has been deleted'}))
+  .catch(err => res.status(500).json({error: err, message: 'there was an error deleting this Order'}))
+})
 
 //protected route that only admin has access to
 router.get('/admin/all', (req, res) => {
