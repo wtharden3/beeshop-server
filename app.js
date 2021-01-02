ad1116dbfa7c379b03380a27da8e16fec5ad56be
Whitney Harden <wtharden320@gmail.com>
2021-01-02 16:35:55 -0500

    added middleware headers file and deleted middleware helpers file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 //bring in db
 const db = require('./db');
 const app = express();
+app.use(require("./middleware/headers"));
 //bring in controllers - defaults to index.js
 const controllers = require('./controllers');
 app.use(express.json())
